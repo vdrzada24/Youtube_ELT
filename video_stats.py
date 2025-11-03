bb915f746dd8a7906f1830b12674db34ece60d6d
Vedant <122686059+vdrzada24@users.noreply.github.com>
2025-11-04 00:08:40 +0530

    added python and update .gitignore file

--- video_stats.py
+++ video_stats.py
@@ -3,10 +3,14 @@ import json
 import os
 from dotenv import load_dotenv
 
+from datetime import date
+
+
 load_dotenv(dotenv_path = "./.env")
 
 Api_key = os.getenv("Api_key")
 Channel_handle = "MrBeast"
+maxResults = 50
 
 def get_playlist_id():
 
@@ -32,5 +36,105 @@ def get_playlist_id():
     except requests.exceptions.RequestException as e:
         raise e
     
+def get_video_id(playlistId):
+
+    video_ids = []
+
+    page_token = None
+
+    base_url = f"https://youtube.googleapis.com/youtube/v3/playlistItems?part=contentDetails&{maxResults}=1&playlistId={playlistId}&key={Api_key}"
+
+    try:
+
+        while True:
+            
+            url = base_url
+
+            if page_token:
+
+                url += f"&pageToken={page_token}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items', []):
+                video_id = item['contentDetails']['videoId']
+                video_ids.append(video_id)
+
+            page_token = data.get('nextPageToken')
+
+            if not page_token:
+                break
+
+        return video_ids
+
+    except requests.exceptions.RequestException as e:
+        raise e
+    
+def extract_video_data(video_ids):
+
+    extracted_data = []
+
+    def batch_list(video_id_lst, batch_size):
+        for video_id in range(0,len(video_id_lst),batch_size):
+            yield video_id_lst[video_id: video_id + batch_size]
+            
+
+    try:
+
+        for batch in batch_list(video_ids,maxResults):
+            video_id_str = ",".join(batch)
+
+            url = f"https://youtube.googleapis.com/youtube/v3/videos?part=contentDetails&part=snippet&part=statistics&id={video_id_str}&key={Api_key}"
+
+            response = requests.get(url)
+
+            response.raise_for_status()
+
+            data = response.json()
+
+            for item in data.get('items',[]):
+                video_id = item['id']
+                snippet = item['snippet']
+                contentDetails = item['contentDetails']
+                statistics = item['statistics']
+
+                video_data = {
+                    "video_id" : video_id,
+                    "title" : snippet['title'],
+                    "publishedAt" : snippet['publishedAt'],
+                    "duration" : contentDetails['duration'],
+                    "viewCount" : statistics.get('viewCount',None),
+                    "likeCount" : statistics.get('likeCount', None),
+                    "commentCount" : statistics.get('commentCount',None)
+
+                }
+
+                extracted_data.append(video_data)
+
+        return extracted_data
+    
+    except requests.exceptions.RequestException as e:
+        raise e
+
+def save_to_json(extracted_data):
+
+    file_path = f"./data/YT_data_{date.today()}.json"
+
+    # using context maanagers to writing in file.
+
+    with open(file_path,"w",encoding="utf-8") as json_output:
+        json.dump(extracted_data,json_output,indent=4,ensure_ascii=False)
+
+
+
+    
+
 if __name__ == '__main__':
-    get_playlist_id()
+    playlistId = get_playlist_id()
+    video_ids = get_video_id(playlistId)
+    video_data= extract_video_data(video_ids)
+    save_to_json(video_data)
